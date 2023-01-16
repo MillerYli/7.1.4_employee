b8908445ac5df3ef5434720d1a6b47cedf90da82
Yulia Miller <miller82.98@mail.ru>
2023-01-16 10:24:06 -0800

    Добавьте файлы проекта.

---
+++ 7.1.4. employee/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class MyClass
+{
+
+}
+
+class Employee
+{
+    public string Name;
+    public int Age;
+    public int Salary;
+}
+class ProjectManager: Employee
+{
+    public string ProjectName;
+}
+
+class Developer: Employee
+{
+    public string ProgrammingLanguage;
+}
